47343071cb7c6967b3e6f3b8379657f82fee38d0
royeradames <royeraadames@gmail.com>
2020-09-03 20:01:51 -0400

    fix incurret api url

--- users-app/src/api/Users.jsx
+++ users-app/src/api/Users.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const getUserById = (id) => {
     debugger
-    return axios.get(`${process.env.APIURL || 'http://localhost:8080'}/api/users/${id}`)
+    return axios.get(`https://royer-adames-node-api1-project.herokuapp.com/api/users/${id}`)
       .then(resp => {
         debugger
         return resp
